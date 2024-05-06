f759388418d294de381e7e89ae8610aa03342b79
nikush0 <bochkovskiynikita@gmail.com>
2024-05-06 12:43:18 +0300

    hw

---
+++ vite-project/src/components/FriendList/FriendList.jsx
@@ -0,0 +1,20 @@
+import FriendListItem from './FriendListItem';
+import css from './FriendList.module.css';
+function FriendList({ friends }) {
+  return (
+    <ul className={css.list}>
+      {friends.map(friend => {
+        return (
+          <li className={css.line} key={friend.id}>
+            <FriendListItem
+              name={friend.name}
+              avatar={friend.avatar}
+              isOnline={friend.isOnline}
+            />
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+export default FriendList;
